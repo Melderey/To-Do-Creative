69567c04fe93e7f2f4ac15d59b91dfa514477251
Melderey <mimeev2010@gmail.com>
2022-01-06 20:51:20 +0500

    Refactor the visual style

--- src/components/SelectStatusTask/SelectStatusTask.jsx
+++ src/components/SelectStatusTask/SelectStatusTask.jsx
@@ -1,25 +1,21 @@
 import React from "react";
-import "./SelectStatusTask.css";
 
 export default class SelectStatusTask extends React.Component {
   render() {
     const { handlerSelectStatusTask, id, statusTaskColor } = this.props;
 
     return (
-      <>
-        <select
-          value={statusTaskColor}
-          onChange={(e) => handlerSelectStatusTask(e, id)}
-          className="select-render"
-        >
-          <option value={"black"}>Выберите приоритет задачи</option>
-          <option value={"red"}>Срочно, важно</option>
-          <option value={"green"}>Не срочно, важно</option>
-          <option value={"orange"}>Срочно, не важно</option>
-          <option value={"blue"}>Не срочно, не важно</option>
-        </select>
-        <hr />
-      </>
+      <select
+        value={statusTaskColor}
+        onChange={(e) => handlerSelectStatusTask(e, id)}
+        className="select-render"
+      >
+        <option value={"black"}>Выберите приоритет задачи</option>
+        <option value={"red"}>Срочно, важно</option>
+        <option value={"green"}>Не срочно, важно</option>
+        <option value={"orange"}>Срочно, не важно</option>
+        <option value={"blue"}>Не срочно, не важно</option>
+      </select>
     );
   }
 }
